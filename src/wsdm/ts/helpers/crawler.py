7d66ef9c0b163da45e816a6a09e3589f9409f7ae
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-05 23:13:14 +0300

    Implement crawler to download wikipedia pages for people

--- src/wsdm/ts/helpers/crawler.py
+++ src/wsdm/ts/helpers/crawler.py
@@ -1,5 +1,26 @@
+import urllib.request
+import urllib.parse
+
+def write_wikipedia_html_content(personName):
+    url = 'http://en.wikipedia.org/wiki/' + urllib.parse.quote(personName)
+    with urllib.request.urlopen(url) as response:
+        html_content = response.read().decode("utf-8")
+
+        try:
+            htmlFile = open('../../../../_DATA/wikipedia/' + personName + '.html', encoding='utf8', mode='x')
+            htmlFile.write(html_content)
+            htmlFile.close()
+            print(personName + ' added!')
+        except FileExistsError as e:
+            pass
+
+
 f = open('../../../../_DATA/nomenclatures/persons.txt', encoding='utf8', mode='r')
 
 for i, line in enumerate(f):
-    name = line.split('	', 1 )[0]
-    print(name)
+    name = line.split('	', 1)[0]
+    write_wikipedia_html_content(name)
+    if i > 20:
+        break
+
+f.close()
