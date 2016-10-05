ad884a84cfc69ca992fb318caa7603bf9165e581
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-05 23:24:27 +0300

    Split getContent & writeToFile methods

--- src/wsdm/ts/helpers/crawler.py
+++ src/wsdm/ts/helpers/crawler.py
@@ -1,16 +1,17 @@
 import urllib.request
 import urllib.parse
 
-def write_wikipedia_html_content(personName):
+def get_wikipedia_html_content(personName):
     url = 'http://en.wikipedia.org/wiki/' + urllib.parse.quote(personName)
     with urllib.request.urlopen(url) as response:
-        html_content = response.read().decode("utf-8")
+        return response.read().decode("utf-8")
 
+def write_content_to_file(content, person_name):
         try:
-            htmlFile = open('../../../../_DATA/wikipedia/' + personName + '.html', encoding='utf8', mode='x')
-            htmlFile.write(html_content)
-            htmlFile.close()
-            print(personName + ' added!')
+            file = open('../../../../_DATA/wikipedia/' + person_name + '.txt', encoding='utf8', mode='x')
+            file.write(content)
+            file.close()
+            print(person_name + ' added!')
         except FileExistsError as e:
             pass
 
@@ -18,9 +19,10 @@ def write_wikipedia_html_content(personName):
 f = open('../../../../_DATA/nomenclatures/persons.txt', encoding='utf8', mode='r')
 
 for i, line in enumerate(f):
-    name = line.split('	', 1)[0]
-    write_wikipedia_html_content(name)
-    if i > 20:
+    person_name = line.split('	', 1)[0]
+    html_content = get_wikipedia_html_content(person_name)
+    write_content_to_file(html_content, person_name)
+    if i > 30:
         break
 
 f.close()
