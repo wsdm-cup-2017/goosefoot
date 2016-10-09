49f7f15a2cca1ddb1e1d9047520610555d416118
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-09 13:05:19 +0300

    Change MySentences class in word2vecGenerator to walk through folders, not files

--- src/wsdm/ts/helpers/word2vecGenerator.py
+++ src/wsdm/ts/helpers/word2vecGenerator.py
@@ -1,18 +1,26 @@
 import gensim, logging
+import os
 import countries
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+
 class MySentences(object):
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self, dirname):
+        self.dirname = dirname
 
     def __iter__(self):
-        for line in open(self.file_name, encoding='utf8'):
-            line = countries.process_line(line)
-            yield line.split()
+        for fname in os.listdir(self.dirname):
+            for line in open(os.path.join(self.dirname, fname), encoding='utf8'):
+                line = countries.process_line(line)
+                yield line.split()
 
-sentences = MySentences('../../../../_DATA/wiki_sentences/wiki-sentences.txt')
-model = gensim.models.Word2Vec(sentences, workers=4)
+persons_folder = MySentences('../../../../_DATA/persons')
+professions_folder = MySentences('../../../../_DATA/professions')
+
+wiki_sentences_folder = MySentences('../../../../_DATA/wiki_sentences')
+
+model = gensim.models.Word2Vec(persons_folder, workers=4, min_count=1)
+model.train(professions_folder)
 
 model.save('../../../../_DATA/word2vec_model.txt')
