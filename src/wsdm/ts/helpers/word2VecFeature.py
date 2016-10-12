24999e13b5a0c07edc353eb8fcbcb12f89cca445
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-12 11:42:02 +0300

    Implement word2vec feature

---
+++ src/wsdm/ts/helpers/word2VecFeature.py
@@ -0,0 +1,27 @@
+import gensim
+
+import persons
+import countries
+import professions
+
+def load_module():
+    global model
+    model = gensim.models.Word2Vec.load('../../../../_DATA/word2vec_model.txt')
+
+def find_profession_similarity(person_name, profession):
+    global model
+
+    person_name = persons.remove_spaces(person_name)
+    profession_words = professions.get_similarity_words(profession)
+    result = 0;
+    for word in profession_words:
+        result += model.similarity(person_name, word)
+    result /= len(profession_words)
+    return result
+
+def find_nationality_similarity(person_name, nationality):
+    global model
+
+    person_name = persons.remove_spaces(person_name)
+    nationality = countries.remove_spaces(nationality)
+    return model.similarity(person_name, nationality)
