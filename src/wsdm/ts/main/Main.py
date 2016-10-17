d9f08f0649a178108953f00519e46c31cf754105
Dimitar Alexandrov <dimitar.alexandrov@fadata.eu>
2016-10-18 00:02:32 +0300

    Added Main.check_file_content_type which returns if the file contains data for people or professions. Constants for NATIONALITY and PROFESSION have to be added.

--- src/wsdm/ts/main/Main.py
+++ src/wsdm/ts/main/Main.py
@@ -1,12 +1,25 @@
 import sys
 import Config
 
+from src.wsdm.ts.helpers.countries import countries
+
+''' Returns the content type in the corresponding file (PERSON or NATIONALITY).'''
+def check_file_content_type(file_path):
+    with open(file_path, 'r') as f:
+        first_line = f.readline()
+        profession_or_nationality = first_line.split('\t')[1]
+        f.close()
+    if profession_or_nationality.lower() in countries.capitals_dict:
+        return 'NATIONALITY'
+    return 'PROFESSION'
 
 def main(argv):
     inputFile, outputFile = Config.getIOFiles(argv)
     print('Input file is: ', inputFile)
     print('Output file is: ', outputFile)
+    print('Content type is: ' + check_file_content_type(inputFile))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
 
