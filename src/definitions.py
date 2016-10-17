eacb95d274d7800823e146e765098fd0787a05da
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-17 14:37:10 +0300

    Change word2vec model dir

--- src/definitions.py
+++ src/definitions.py
@@ -2,10 +2,10 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, os.pardir, "_DATA")
-WORD2VEC_MODEL_DIR = os.path.join(DATA_DIR, "word2vec_model.txt")
 NOMENCLATURES_DIR = os.path.join(DATA_DIR, "nomenclatures")
 PROFESSIONS_DIR = os.path.join(DATA_DIR, "professions")
 TEST_DIR = os.path.join(DATA_DIR, "test_sets")
 TRAINING_DIR = os.path.join(DATA_DIR, "training_sets")
 
 PERSONS_DIR = 'D:/WSDM_Persons/persons/'
+WORD2VEC_MODEL_DIR = 'D:/WSDM_Persons/word2vec/word2vec_model.txt'
