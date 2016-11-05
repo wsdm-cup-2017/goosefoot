67e8fe1a55ffaccb3c837ca849491e319d2c1ed8
Dimitar Alexandrov <dimitar.alexandrov@fadata.eu>
2016-11-05 13:32:46 +0200

    Added method, which returns the content type of the -i file, based on its first row.

--- src/wsdm/ts/main/Main.py
+++ src/wsdm/ts/main/Main.py
@@ -1,6 +1,7 @@
 import sys
 import Config
 
+
 from src.wsdm.ts.helpers.countries import countries
 
 ''' Returns the content type in the corresponding file (PERSON or NATIONALITY).'''
@@ -9,17 +10,29 @@ def check_file_content_type(file_path):
         first_line = f.readline()
         profession_or_nationality = first_line.split('\t')[1]
         f.close()
-    if profession_or_nationality.lower() in countries.capitals_dict:
+
+    if profession_or_nationality.lower().strip() in countries.countries_dict.values():
         return 'NATIONALITY'
     return 'PROFESSION'
 
+
+
 def main(argv):
+
     inputFile, outputFile = Config.getIOFiles(argv)
     print('Input file is: ', inputFile)
     print('Output file is: ', outputFile)
     print('Content type is: ' + check_file_content_type(inputFile))
 
+
+
 if __name__ == '__main__':
+    '''
+    For unit test purposes, main can be called with arguments, different than sys.argv, e.g.:
+
+    main(['-i','D:/eclipse/workspaces/wsdm_2017/triple-scoring/goosefoot/_DATA/test_sets/nationality.kb','-o' 'D:/temp/asd.txt'])'''
+
     main(sys.argv[1:])
+
 
 
