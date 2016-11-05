d30d8fccb1dcd704556b4282224c0b195b066ab4
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-05 15:16:58 +0200

    Revert countries to be in PascalCase (they are already used this way!)

--- src/wsdm/ts/main/Main.py
+++ src/wsdm/ts/main/Main.py
@@ -1,8 +1,7 @@
 import sys
-import Config
-
-
+import src.wsdm.ts.main.Config as Config
 from src.wsdm.ts.helpers.countries import countries
+
 
 ''' Returns the content type in the corresponding file (PERSON or NATIONALITY).'''
 def check_file_content_type(file_path):
@@ -11,14 +10,13 @@ def check_file_content_type(file_path):
         profession_or_nationality = first_line.split('\t')[1]
         f.close()
 
-    if profession_or_nationality.lower().strip() in countries.countries_dict.values():
+    if profession_or_nationality.strip() in countries.countries_dict.values():
         return 'NATIONALITY'
     return 'PROFESSION'
 
 
 
 def main(argv):
-
     inputFile, outputFile = Config.getIOFiles(argv)
     print('Input file is: ', inputFile)
     print('Output file is: ', outputFile)
