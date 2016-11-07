b656b6bad8c7f6ec69f1c6d9cc75615c09b0f75f
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-07 11:55:24 +0200

    Add tfidf professions dictionary & optimize tf idf words extractor

--- src/wsdm/ts/helpers/tfidf/tfidf_words_extractor.py
+++ src/wsdm/ts/helpers/tfidf/tfidf_words_extractor.py
@@ -12,16 +12,16 @@ def get_lower_words(text):
     return re.findall(r"\b[a-z]+\b", text)
 
 
-def tf(word, words):
-    return words.count(word) / len(words)
+def tf(word, profession, words_dict, doc_words_count):
+    return words_dict[word][profession] / doc_words_count
 
 
 def idf(word, total_count, words_dict):
-    return math.log(total_count / (1 + words_dict[word]))
+    return math.log(total_count / (1 + len(words_dict[word])))
 
 
-def tfidf(word, doc_words, total_count, words_dict):
-    return tf(word, doc_words) * idf(word, total_count, words_dict)
+def tfidf(word, profession, total_count, words_dict, doc_words_count):
+    return tf(word, profession, words_dict, doc_words_count) * idf(word, total_count, words_dict)
 
 
 def get_professions_words_list():
@@ -29,7 +29,7 @@ def get_professions_words_list():
         for line in f:
             profession = line.rstrip()
             with open(os.path.join(TFIDF_PROFESSIONS_DIR, profession + '.txt'), encoding='utf8', mode='r') as prof_file:
-                yield get_lower_words(prof_file.read())
+                yield profession, get_lower_words(prof_file.read())
 
 
 def get_professions_list():
@@ -37,15 +37,14 @@ def get_professions_list():
 
 
 def init_words_dict():
-    result = defaultdict(int)
+    result = defaultdict(dict)
     index = 1
-    for doc_words in get_professions_words_list():
+    for profession, doc_words in get_professions_words_list():
         document_dict = Counter(doc_words)
         for word in document_dict:
             if len(result) % 20000 == 0:
                 print(index, "document:", len(result), "words", time.strftime("%H:%M:%S"))
-            # result[word] += document_dict[word]
-            result[word] += 1
+            result[word][profession] = document_dict[word]
         index += 1
 
     return result
@@ -55,17 +54,16 @@ def main():
     professions = get_professions_list()
     words_dict = init_words_dict()
 
-    index = 0
     print("{")
-    for doc_words in get_professions_words_list():
-        scores = {word: tfidf(word, doc_words, len(professions), words_dict) for word in doc_words}
+    for profession, doc_words in get_professions_words_list():
+        scores = {word: tfidf(word, profession, len(professions), words_dict, len(doc_words)) for word in doc_words}
 
         sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)[:20]
-        print("'{0}': {{".format(professions[index]))
+        print("# {0}".format(time.strftime("%H:%M:%S")))
+        print("'{0}': {{".format(profession))
         for word, score in sorted_words:
             print("\t'{0}': {1},".format(word, round(score, 8)))
-        print('}')
-        index += 1
+        print('},')
     print("}")
 
 if __name__ == '__main__':
