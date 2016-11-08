41d3f1355b74d36bb2a2986fe1ffb52798030d85
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-08 20:25:34 +0200

    Change WSDM_BIG_DATA to be relative to the project (deployment purposes)

--- src/definitions.py
+++ src/definitions.py
@@ -7,7 +7,7 @@ PROFESSIONS_DIR = os.path.join(DATA_DIR, "professions")
 TEST_DIR = os.path.join(DATA_DIR, "test_sets")
 TRAINING_DIR = os.path.join(DATA_DIR, "training_sets")
 
-BIG_DATA_DIR = 'D:/WSDM_BIG_DATA/'
+BIG_DATA_DIR = os.path.join(ROOT_DIR, os.pardir, os.pardir, "WSDM_BIG_DATA")
 PERSONS_DIR = os.path.join(BIG_DATA_DIR, "persons")
 TFIDF_PROFESSIONS_DIR = os.path.join(BIG_DATA_DIR, "tfidf_professions")
 WORD2VEC_MODEL_PATH = os.path.join(BIG_DATA_DIR, "word2vec/word2vec_model.txt")
