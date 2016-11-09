c969809619801ae052d33a1e8ddd290e9beda2f0
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-09 10:10:20 +0200

    Add negative feature

---
+++ src/wsdm/ts/features/negativeFeature.py
@@ -0,0 +1,9 @@
+import src.wsdm.ts.helpers.train.common_train as common_train
+
+
+def is_nationality_negative(person, nationality):
+    return common_train.is_nationality_negative(person, nationality)
+
+
+def is_profession_negative(person, profession):
+    return common_train.is_profession_negative(person, profession)
