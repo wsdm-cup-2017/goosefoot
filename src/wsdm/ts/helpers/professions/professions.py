7f0be4ba6e49382bb61cce793ae556a86faa8931
Dimitar Alexandrov <dimitar.alexandrov@fadata.eu>
2016-11-13 22:43:54 +0200

    Based on the already added dictionary (profession_synonyms_map) the function get_similar_professions returns synonyms for professions, which are not found in profession_synonyms_map.

--- src/wsdm/ts/helpers/professions/professions.py
+++ src/wsdm/ts/helpers/professions/professions.py
@@ -206,10 +206,10 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'activist':'revolutionary;advocate;opponent',
 'actor':'star;artist;character;clown;player;villain;comedian;entertainer;thespian;impersonator;amateur;ham;lead;extra;understudy;mimic;foil;mime;idol;stooge;bit player;ingenue;performer;trouper;stand-in;walk-on;play-actor;pantomimist;ventriloquist;headliner;barnstormer;soubrette;hambone;ingénue;straight person;thesp',
 'alchemist':'warlock;diviner;seer;enchanter;charmer;shaman;conjurer;medium;witch;soothsayer;sorceress;clairvoyant;magician;fortune-teller;occultist;necromancer;thaumaturge;augurer',
-'american football player':' ',
+'american football player':'',
 'animator':'illustrator;artisan;craftsman;landscapist;miniaturist;dauber',
 'announcer':'disc jockey;reporter;newscaster;talker;broadcaster;anchorperson;communicator;dj;telecaster;vj;deejay;leader of ceremonies',
-'anthropologist':' ',
+'anthropologist':'',
 'architect':'designer;artist;builder;creator;planner;engineer;inventor;originator;maker;prime mover;draftsperson;master builder',
 'art critic':'craft;profession;dexterity;artistry;knowledge;adroitness;ingenuity;mastery;facility;trade;imagination;know-how;aptitude;expertise;inventiveness;knack;method;virtuosity;craftsmanship',
 'art director':'craft;profession;dexterity;artistry;knowledge;adroitness;ingenuity;mastery;facility;trade;imagination;know-how;aptitude;expertise;inventiveness;knack;method;virtuosity;craftsmanship',
@@ -222,13 +222,13 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'author':'producer;creator;writer;columnist;journalist;composer;poet;reporter;ghost;originator;playwright;scribe;biographer;scribbler;ghostwriter;wordsmith;essayist;ink slinger',
 'bandleader':'composer;conductor;master;teacher',
 'barrister':'advocate;counsel;counselor;lawyer;solicitor',
-'baseball manager':' ',
-'baseball player':' ',
+'baseball manager':'',
+'baseball player':'',
 'basketball player':'ball;hoops',
-'bassist':' ',
+'bassist':'',
 'biologist':'environmentalist;conservationist;zoologist;ecologist;botanist;preservationist',
 'bishop':'administrator;pontiff;pope;cleric;patriarch;director;priest;prelate;angel;metropolitan;primate;cap;coadjutor;overseer;diocesan;suffragan;archer;mitre;miter;berretta;abba',
-'bodybuilder':' ',
+'bodybuilder':'',
 'book editor':'essay;album;novel;publication;dictionary;pamphlet;text;work;manual;textbook;fiction;volume;edition;magazine;booklet;brochure;writing;copy;tome;lexicon;periodical;portfolio;primer;dissertation;opus;handbook;reader;roll;thesaurus;tract;compendium;bible;treatise;omnibus;leaflet;encyclopedia;scroll;monograph;paperback;hardcover;nonfiction;speller;folio;codex;atlas;quarto;reprint;octavo;offprint;bestseller;opuscule;preprint;softcover;vade mecum',
 'botanist':'environmentalist;conservationist;zoologist;ecologist;biologist;preservationist',
 'broadcaster':'disc jockey;reporter;newscaster;talker;anchorperson;communicator;dj;telecaster',
@@ -237,9 +237,9 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'cantor':'singer;leader;precentor;hazan;soloist;chanter;vocalist',
 'carpenter':'laborer;builder;worker;artisan;chips;craftsperson;woodworker;cabinetmaker;carps;joiner;mason',
 'cartoonist':'illustrator;artist;caricaturist;gagster',
-'choreographer':' ',
+'choreographer':'',
 'choreography':'dance',
-'cinematographer':' ',
+'cinematographer':'',
 'civil servant':'local;governmental;domestic;national;civilian;home',
 'coach':'trainer;teacher;mentor;tutor;skipper;educator',
 'comedian':'entertainer;comic;humorist;clown;actor;stooge;wag;jester;jokester;joker;wit;zany;laugh;card;droll;top banana;farceur;quipster;banana;cutup;merry-andrew;million laughs;stand-up comic;wisecracker',
@@ -254,11 +254,11 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'dancer':'ballet dancer;danseuse;danseur;hoofer;chorus girl;prima ballerina',
 'diplomat':'expert;mediator;envoy;agent;negotiator;representative;minister;ambassador;go-between;emissary;plenipotentiary;moderator;legate;conciliator;tactician',
 'disc jockey':'sphere;ring;perimeter;revolution;globe;circus;crown;amphitheater;disk;enclosure;belt;tire;record;bowl;periphery;stadium;hoop;wheel;wreath;horizon;circlet;band;aureole;compass;coil;lap;halo;cycle;circumference;ringlet;bracelet;orbit;corona;meridian;turn;vortex;circuit;round;equator;cirque;cordon;ecliptic;zodiac;colure;full turn;parallel of latitude',
-'drummer': ' ',
-'economist':  ' ',
+'drummer': '',
+'economist':  '',
 'editor':'proofreader;copyreader;newspaperwoman;copyholder;newspaperman;rewriter;reviser;deskman;blue-penciler',
 'educator':'lecturer;professor;instructor;trainer;coach;dean;monitor;tutor;mentor;schoolteacher;educationist;department head',
-'electrical engineer': ' ',
+'electrical engineer': '',
 'electronic musician':'computerized;anodic',
 'engineer':'designer;builder;director;surveyor;manager;planner;architect;inventor;originator;schemer;manipulator;techie;sights;contriver;deviser;technie',
 'entertainer':'artist;player',
@@ -279,11 +279,11 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'financial adviser':'fiscal;economic;commercial;monetary;banking;business',
 'fisherman':'trawler;fisher;lobsterman;troller',
 'football player':'soccer;rugby',
-'footballer':' ',
+'footballer':'',
 'friar':'priest;abbot;solitary;cenobite;recluse;brother;ascetic;religious;monastic;hermit;eremite;anchorite',
 'game show host':'gallant;dogged;bold;hardy;inclined;disposed;prepared;spirited;eager;interested;ready;persevering;heroic;courageous',
-'guitarist':' ',
-'harpsichordist':' ',
+'guitarist':'',
+'harpsichordist':'',
 'historian':'professor;writer;teacher;chronicler;historiographer;annalist',
 'humorist':'entertainer;clown;jester;joker;wit;comic;jokester;card;satirist;comedienne;cutup;jokesmith',
 'ice hockey player':'chunk;glaze;hail;crystal;iceberg;icicle;glacier;sleet;diamonds;hailstone;permafrost;floe',
@@ -302,10 +302,10 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'legislator':'administrator;deputy;lawmaker;member;representative;senator;leader;parliamentarian;council member;lawgiver;aldermember;assemblymember',
 'librarian':'curator;cataloger',
 'librettist':'author;writer;dramatist;scenarist;tragedian;dramaturge',
-'lifeguard':' ',
+'lifeguard':'',
 'lyricist':'poet;musician;composer;songwriter;lyrist;music writer',
 'manager':'administrator;official;supervisor;producer;executive;boss;director;officer;superintendent;organizer;head;comptroller;conductor;exec;governor;proprietor;overseer;straw boss;controller;handler;head person;slavedriver;zookeeper',
-'mathematician':' ',
+'mathematician':'',
 'media proprietor':'news;television;radio;correspondence;disclosure;cable;announcement;intelligence;communications;expression;publishing;announcing',
 'merchant':'shopkeeper;trafficker;vendor;trader;broker;seller;operator;shipper;dealer;tycoon;businessperson;storekeeper;salesperson;exporter;retailer;wholesaler;sender;handler;jobber;consigner;marketer;tradesperson',
 'meteorologist':'witch;seer;prognosticator;prophesier;diviner;medium;sibyl;astrologer;forecaster;reader;wizard;bard;fortuneteller;soothsayer;oracle;augur;auspex;magus;clairvoyant;sorcerer;ovate;palmist;druid;predictor',
@@ -313,17 +313,17 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'military officer':'army;fighting;militant;martial;combatant;aggressive;armed;combative;warlike;militaristic;soldierly;warmongering',
 'missionary':'evangelist;pastor;preacher;messenger;clergy;teacher;apostle;missioner;promoter;herald;propagandist;minister;clergyperson;converter;revivalist;proselytizer',
 'model':'miniature;exemplary;perfect;dummy;classical;classic;standard;facsimile;imitation;copy;representative;archetypal;commendable;flawless;illustrative',
-'multi-instrumentalist':' ',
+'multi-instrumentalist':'',
 'music arranger':'soul;rap;rock;melody;piece;singing;tune;hymn;modern;classical;song;bop;chamber;jazz;measure;fusion;refrain;air;strain;harmony;swing;acoustic;folk;hard rock;instrumental;popular;opera;ragtime;plainsong;bebop;a cappella;heavy metal;rock and roll',
 'music artist':'soul;rap;rock;melody;piece;singing;tune;hymn;modern;classical;song;bop;chamber;jazz;measure;fusion;refrain;air;strain;harmony;swing;acoustic;folk;hard rock;instrumental;popular;opera;ragtime;plainsong;bebop;a cappella;heavy metal;rock and roll',
 'music director':'soul;rap;rock;melody;piece;singing;tune;hymn;modern;classical;song;bop;chamber;jazz;measure;fusion;refrain;air;strain;harmony;swing;acoustic;folk;hard rock;instrumental;popular;opera;ragtime;plainsong;bebop;a cappella;heavy metal;rock and roll',
 'musician':'conductor;artist;virtuoso;player;composer;entertainer;performer;soloist;instrumentalist;vocalist;artiste;diva',
 'music producer':'soul;rap;rock;melody;piece;singing;tune;hymn;modern;classical;song;bop;chamber;jazz;measure;fusion;refrain;air;strain;harmony;swing;acoustic;folk;hard rock;instrumental;popular;opera;ragtime;plainsong;bebop;a cappella;heavy metal;rock and roll',
-'neurologist':' ',
+'neurologist':'',
 'novelist':'writer;storyteller;author;fictionist',
 'orator':'preacher;lecturer;rhetorician;declaimer;public speaker;sermonizer;lector;reciter;pontificator',
 'orchestrator':'instigator;troublemaker;mastermind;chieftain;head;captain;spokesperson;ruler;general;inciter;chief;agitator;commander;skipper;president;boss;brains;head honcho',
-'organist':' ',
+'organist':'',
 'pastor':'cleric;vicar;preacher;priest;minister;rector;divine;parson;ecclesiastic;shepherd;reverend',
 'peace activist':'reconciliation;accord;love;unity;truce;friendship;conciliation;concord;union;amity;treaty;neutrality;cessation;unanimity;order;armistice;pacification;pacifism',
 'performance artist':'work;act;achievement;conduct;completion;enforcement;fruition;consummation;administration;attainment;exploit;fulfillment;realization;discharge;execution;feat;doing;pursuance;carrying out',
@@ -332,14 +332,14 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'photographer':'paparazzo;photojournalist;shutterbug',
 'physician':'specialist;doctor;quack;medic;intern;healer;general practitioner;surgeon;md;doc;bones;sawbones',
 'physicist':'analyst;expert;examiner;prober;chemist;tester',
-'pianist':' ',
+'pianist':'',
 'pilot':'flier;navigator;captain;aviator;leader;scout;jockey;dean;conductor;lead;bellwether;director;ace;flyer;guide;eagle;aerialist;coxswain;steerer;aeronaut;doyen/doyenne',
 'pin-up girl':'babe;angel;doll;broad;honey;chick;bathing beauty;beauty queen;cover girl;sex kitten;tomato;centerfold;peach;fox;cupcake;bunny;cutie;cutie-pie;dollface;dream girl;dreamboat',
-'playback singer':' ',
+'playback singer':'',
 'playwright':'author;writer;dramatist;scenarist;librettist;tragedian',
 'poet':'lyricist;writer;dramatist;author;artist;dilettante;rhymer;bard;versifier;maker;poetaster;parodist;lyrist;librettist;odist',
 'police officer':'detective;force;law enforcement;man;corps;bluecoat;pig;blue;law;badge;patrolman;bear;heat;bull;cop;bobby;constable;fuzz;gumshoe;copper;constabulary;fed;oink;officers;narc;flatfoot;gendarme;boy scout;county mounty',
-'political activist': ' ',
+'political activist': '',
 'politician':'lawmaker;senator;leader;legislator;partisan;boss;speaker;orator;president;chieftain;public servant;officeholder;grandstander;democrat;republican;baby-kisser;congressperson;handshaker',
 'polymath':'educated;scientific;studied;scholarly;accomplished;sound;experienced;versed;cultured;lettered;cultivated;grave;studious;well-grounded;well-read;well-rounded;well-educated;grounded;posted;in the know;pansophic;philosophic;professorial',
 'pornographic actor':'immoral;lewd;salacious;indecent;sexy',
@@ -353,12 +353,12 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'psychoanalyst':'therapist;analyst;shrink;psychotherapist',
 'psychologist':'doctor;therapist;psychoanalyst;shrink;psychotherapist;clinician',
 'public speaker':'urban;mutual;civic;civil;national;communal;governmental;social;popular;universal;city;metropolitan;government;country;free;state;common;open;accessible;municipal;unrestricted;widespread;conjoint;open-door;federal;conjunct;free to all;intermutual;not private;without charge',
-'publisher':  ' ',
+'publisher':  '',
 'rabbi':'priest;teacher;master;rabbin',
 'racing driver':'hurrying;flying;running;fast;dashing;swift;rushing;speedy;darting;sailing;zooming;whisking;hastening;whizzing;tearing;galloping',
 'radio personality':'transmission;receiver;wireless;walkman;telegraphy;radiotelegraphy;telephony;radiotelegraph;marconi;radiotelephone;am-fm;cb',
 'radio producer':'transmission;receiver;wireless;walkman;telegraphy;radiotelegraphy;telephony;radiotelegraph;marconi;radiotelephone;am-fm;cb',
-'rapper':' ',
+'rapper':'',
 'record producer':'story;evidence;note;file;testimony;report;document;transcript;history;inscription;manuscript;memorandum;memo;remembrance;archive;witness;memorial;log;script;writing;annals;archives;memoir;trace;journal;diary;legend;chronicle;register;jacket;almanac;monument;directory;entry;minutes;scroll;track record;documentation;transcription;registry;comic book;paper trail;swindle sheet;written material',
 'revolutionary':'subversive;radical;insurgent;rebel;rioting;anarchistic',
 'rodeo clown':'festival;roundup;exhibition;competition;enclosure',
@@ -369,7 +369,7 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'science writer':'discipline;information;art;technique;system;learning;skill;education;lore;scholarship;erudition;branch;wisdom;body of knowledge',
 'scientist':'analyst;expert;examiner;prober;physicist;chemist;tester;lab technician',
 'screenwriter':'correspondent;author;reporter;critic;columnist;poet;novelist;dramatist;journalist;contributor;scribe;editor;biographer;essayist;wordsmith;scribbler;stenographer;ghostwriter;freelancer;stringer;newspaper person;person of letters;scripter',
-'showgirl':  ' ',
+'showgirl':  '',
 'singer':'artist;musician;voice;troubadour;songbird;songster;minstrel;diva;soloist;crooner;vocalist;artiste;warbler;chorister;accompanist;chanter;nightingale;chanteuse',
 'singer-songwriter':'writer;songster',
 'soccer player':'rugby;american football',
@@ -377,19 +377,19 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'soldier':'fighter;mercenary;guerrilla;veteran;guard;officer;volunteer;marine;pilot;warrior;cadet;infantry;recruit;private;gunner;scout;rank;warmonger;paratrooper;trooper;commando;draftee;musketeer;conscript;gi;green beret;airforce member',
 'songwriter':'poet;musician;composer;lyrist',
 'sound sculptor':'vigorous;sturdy;solid;vibrant;safe;thorough;flawless;sane;stable;robust;intact;firm;entire;right;fit;well;hale;perfect;total;whole',
-'speechwriter':' ',
+'speechwriter':'',
 'spokesperson':'deputy;delegate;champion;mediator;prophet;mouthpiece;agent;protagonist;mouth;representative;speaker;substitute;talker;stand-in;prolocutor',
 'statesman':'lawmaker;politician;legislator',
 'surveyor':'mapmaker;cartographer;assessor;measurer',
-'swimmer':' ',
+'swimmer':'',
 'talk show host':'lecture;dissertation;homily;exhortation;harangue;descant;recitation;declamation;spiel;discourse;allocution;oration;monologue;epilogue;sermon;disquisition;prelection;screed;peroration;expatiation;chalk talk',
 'teacher':'lecturer;supervisor;scholar;assistant;professor;tutor;coach;educator;instructor;trainer;adviser;preceptor;pundit;mentor;pedagogue;teach;disciplinarian;guide;schoolteacher;faculty member;abecedary',
 'television director':'tv set;box;station;video;eye;tv;tube;receiver;baby-sitter;boob tube;idiot box;small screen;audio;telly;vid',
 'television presenter':'tv set;box;station;video;eye;tv;tube;receiver;baby-sitter;boob tube;idiot box;small screen;audio;telly;vid',
 'television producer':'tv set;box;station;video;eye;tv;tube;receiver;baby-sitter;boob tube;idiot box;small screen;audio;telly;vid',
 'television show host':'tv set;box;station;video;eye;tv;tube;receiver;baby-sitter;boob tube;idiot box;small screen;audio;telly;vid',
-'tennis player':' ',
-'tentmaker':' ',
+'tennis player':'',
+'tentmaker':'',
 'theatre director':'troupe;theater;drama;histrionics;dramatics;theatrics;dramaturgy;footlights;histrionism',
 'theatrical producer':'exaggerated;melodramatic;showy;comic;thespian;vaudeville;amateur;ham;show;affected;artificial;ceremonious;histrionic;legitimate;mannered;meretricious;schmaltzy;theatric;campy;operatic;hammy;staged',
 'theologian':'cleric;clergy;philosopher;scholar;divine;curate;ecclesiastic',
@@ -398,18 +398,38 @@ profession_synonyms_map = {'accountant':'analyst;clerk;auditor;bookkeeper;comptr
 'tv editor':'box;baby-sitter;eye;tube;station;video;receiver;tv set;telly;audio;vid',
 'tv personality':'box;baby-sitter;eye;tube;station;video;receiver;tv set;telly;audio;vid',
 'urban planner':'downtown;civil;metropolitan;civic',
-'violinist':' ',
-'violist':' ',
+'violinist':'',
+'violist':'',
 'voice actor':'sound;speech;cry;tone;statement;exclamation;vent;words;song;modulation;articulation;inflection;roar;mutter;delivery;vocalization;shout;call;murmur;intonation;yell;vociferation;utterance;tongue',
 'warrior':'soldier;fighter;hero;champion;combatant;conscript;trooper;gi;battler',
 'writer':'correspondent;author;reporter;critic;columnist;poet;novelist;dramatist;journalist;contributor;scribe;editor;biographer;essayist;screenwriter;wordsmith;scribbler;stenographer;ghostwriter;freelancer;stringer;newspaper person;person of letters;scripter',
 }
+
 def get_similarity_words(profession):
-    result = re.findall(r"[\w]+", profession.lower())
+    return re.findall(r"[\w]+", profession.lower())
 
-    for word in ["in", "the"]:
-        if word in result:
-            result.remove(word)
+def get_similar_professions(profession):
+    profession_synonyms = profession_synonyms_map.get(profession)
 
-    return result
+    if not profession_synonyms:
+        profession_words = profession.split(' ')
+        #the next transformation is relevant only if the
+        #profession consist more than one word, e.g. 'electronical engineer'
+        if len(profession_words) > 1:
+            last_word = profession_words[-1]
+            #search for synonyms of the last word
+            last_word_synonyms = profession_synonyms_map.get(last_word.strip())
+            if not last_word_synonyms:
+                return ''
+            result=''
 
+            #get the profession, without the last word
+            profession_prefix = profession.replace(last_word, '').strip()
+            for last_word_synonym in last_word_synonyms.split(';'):
+                #add the first part of the word + the newly found synonym
+                result+=profession_prefix + ' ' + last_word_synonym + ';'
+
+            return result[:-2]
+        return ''
+    else:
+        return profession_synonyms_map.get(profession)
