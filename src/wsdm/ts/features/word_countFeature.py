c1deae952982322dfce0596abf3025d2bfdbfe15
Dimitar Alexandrov <dimitar.alexandrov@fadata.eu>
2016-11-24 01:42:10 +0200

    Tuning of the word_count feature. It`s important to extend the terms in the nationalities section as it`s not enough and it`s not providing satisfying results. Also, find_similatiry has been implemented.

--- src/wsdm/ts/features/word_countFeature.py
+++ src/wsdm/ts/features/word_countFeature.py
@@ -1,26 +1,12 @@
 import src.wsdm.ts.helpers.persons.persons as p_lib
 from src.wsdm.ts.helpers.tfidf.professions_tfidf_dictionary import PROFESSIONS_DICT
 from src.wsdm.ts.helpers.tfidf.nationalities_tfidf_dictionary import NATIONALITIES_DICT
-import os
 import definitions
-from collections import Counter
-import operator
 import nltk
 import sys
-import codecs
+
 import re
-
-def find_similarity(person_name, term, inputType):
-    return ''
-
-def custom_similarity(person, key, words_dict):
-    result = -1
-    #TODO implement !
-    if result > 7:
-        return 7
-
-    return result
-
+import os
 
 def find_similarity(person_name, term, inputType):
     if inputType == definitions.TYPE_NATIONALITY:
@@ -32,6 +18,34 @@ def find_similarity(person_name, term, inputType):
 
     return custom_similarity(person_name, term, dict)
 
+def custom_similarity(person, key, words_dict):
+    result={}
+    person_file_path = os.path.join(definitions.PERSONS_DIR, p_lib.remove_spaces(person) + ".txt")
+    if(words_dict == NATIONALITIES_DICT):
+        f = open(person_file_path)
+        result = get_person_nationalities(f)
+        f.close()
+    elif words_dict == PROFESSIONS_DICT:
+        f = open(person_file_path)
+        result = get_person_professions(f)
+        f.close()
+    else:
+        TypeError
+
+    if (key in result.keys()):
+        return result[key]
+    return 0
+
+
+def find_similarity(person_name, term, inputType):
+    if inputType == definitions.TYPE_NATIONALITY:
+        dict = NATIONALITIES_DICT
+    elif inputType == definitions.TYPE_PROFESSION:
+        dict = PROFESSIONS_DICT
+    else:
+        raise TypeError
+
+    return custom_similarity(person_name, term.lower(), dict)
 
 def remove_quoted_words(text):
     text=text.lower()
@@ -68,10 +82,13 @@ def get_person_professions(file):
         overview_line = lines[0] + lines[1] + lines[2]
         line_words = nltk.word_tokenize(overview_line.lower())
         if (profession in line_words):
-            result[profession] = profession_majority
+            result[profession.lower()] = profession_majority
             profession_majority-=1
 
     for line in lines:
+        #It`s important to remove all quoted strings as
+        #this may cause problems when searching professions
+        #or nationalities
         line = remove_quoted_words(line)
 
         words = nltk.word_tokenize(line.lower())
@@ -88,9 +105,9 @@ def get_person_professions(file):
                 elif (profession_length > 2):
                     profession_first_word = profession_words[-2]
                     profession_last_word = profession_words[-1]
-
-                if (profession_first_word == word or '''profession_last_word''' == word) and (profession not in result):
-                    result[profession] = profession_majority
+                     #'''or profession_last_word == word'''
+                if (profession_first_word == word) and (profession not in result):
+                    result[profession.lower()] = profession_majority
                     if profession_majority > 0:
                         profession_majority-=1
 
@@ -102,20 +119,20 @@ def get_person_nationalities(file):
     nationality_majority = 7
     from wsdm.ts.helpers.nationalities import nationalities
     for line in lines:
-        words = nltk.word_tokenize(line.lower())
         for person, country in nationalities.nationalities_dict.items():
             if (country not in result) and ((person in line) or (country in line)):
-                result[country] = nationality_majority
+                result[country.lower()] = nationality_majority
                 if nationality_majority > 0:
                     nationality_majority-=1
     return result
 
 
 def main(argv):
-    f = codecs.open('D:/education/FMI_Sofia_University/III_sem/wsdm_2017/data/DATA_2016_10_15/persons/Gowin_Knight.txt', 'r', encoding='utf8')
-    result = get_person_nationalities(f)
-    f.close()
-    print(result)
+    #f = codecs.open('D:/education/FMI_Sofia_University/III_sem/wsdm_2017/data/DATA_2016_10_15/persons/Richard_Séguin.txt', 'r', encoding='utf8')
+    #result = get_person_nationalities(f)
+    print(find_similarity('Richard Séguin', 'Germany', definitions.TYPE_NATIONALITY))
+    #f.close()
+    #print(result)
 
 if __name__ == '__main__':
     main(sys.argv[:])
