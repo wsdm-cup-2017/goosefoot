5228b9ce4c9ec2d134692452dffb1408e9602b4b
Dimitar Alexandrov <dimitar.alexandrov@fadata.eu>
2016-11-24 01:39:11 +0200

    Added the word_count feature in the features factory function.

--- src/wsdm/ts/helpers/regression/regression_utils.py
+++ src/wsdm/ts/helpers/regression/regression_utils.py
@@ -1,9 +1,11 @@
 import numpy as np
 from wsdm.ts.features import tfIdfFeature
+from wsdm.ts.features import word_countFeature
 
 def get_features_values(person_name, term, inputType, w2vecFeature):
     # TODO: Add more features
     return np.array([
         tfIdfFeature.find_similarity(person_name, term, inputType),
-        w2vecFeature.find_similarity(person_name, term, inputType)
+        w2vecFeature.find_similarity(person_name, term, inputType),
+        word_countFeature.find_similarity(person_name, term, inputType)
     ])
