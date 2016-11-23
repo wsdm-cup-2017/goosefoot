3aba8de13aee30ed01c75b2a33daf20107d2d2dc
Dimitar Alexandrov <dimitar.alexandrov@fadata.eu>
2016-11-24 01:36:44 +0200

    Added test class for the word_count feature

---
+++ src/wsdm/ts/features/tests/test_word_count.py
@@ -0,0 +1,13 @@
+
+from src import definitions
+from wsdm.ts.features import word2VecFeature
+from wsdm.ts.features.tests.test_common import calculate_similarities
+
+if __name__ == '__main__':
+
+    print("----- NATIONALITIES -----")
+    calculate_similarities("wsdm_nationality.train", word2VecFeature.find_similarity, definitions.TYPE_NATIONALITY)
+
+    print("\n\n\n\n\n\n")
+    print("----- PROFESSIONS -----")
+    calculate_similarities("wsdm_profession.train", word2VecFeature.find_similarity, definitions.TYPE_PROFESSION)
