adf6434db50dc192fb15741b4e355b1a953f19b6
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-24 12:51:43 +0200

    Remove word count feature from regression

--- src/wsdm/ts/helpers/regression/regression_utils.py
+++ src/wsdm/ts/helpers/regression/regression_utils.py
@@ -6,6 +6,5 @@ def get_features_values(person_name, term, inputType, w2vecFeature):
     # TODO: Add more features
     return np.array([
         tfIdfFeature.find_similarity(person_name, term, inputType),
-        w2vecFeature.find_similarity(person_name, term, inputType),
-        word_countFeature.find_similarity(person_name, term, inputType)
+        w2vecFeature.find_similarity(person_name, term, inputType)
     ])
